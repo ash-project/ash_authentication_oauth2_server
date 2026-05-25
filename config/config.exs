a4d8c361f710e5a050a1cf4834c9fa6c8f3dbd01
Zach Daniel <zach@zachdaniel.dev>
2026-05-25 17:12:27 -0400

    chore: release version v0.1.0

--- config/config.exs
+++ config/config.exs
@@ -7,3 +7,16 @@ import Config
 if Mix.env() == :test do
   import_config "test.exs"
 end
+
+if Mix.env() == :dev do
+  config :git_ops,
+    mix_project: AshAuthentication.Oauth2Server.MixProject,
+    github_handle_lookup?: true,
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/team-alembic/ash_authentication_oauth2_server",
+    # Manage the version in `mix.exs`
+    manage_mix_version?: true,
+    # Manage the version in `README.md` (and any other doc that calls it out)
+    manage_readme_version: "README.md",
+    version_tag_prefix: "v"
+end
